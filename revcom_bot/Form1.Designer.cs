37f64dc981ae5ed43c9f88197cee127ea482398f
Narek Torosyan <narek.email@gmail.com>
2019-11-05 21:10:56 +0400

    yay files

---
+++ revcom_bot/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PrinterBot {
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BtnRun = new System.Windows.Forms.Button();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BtnRun);
+            this.groupBox1.Controls.Add(this.txtKey);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(395, 77);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Token";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // BtnRun
+            // 
+            this.BtnRun.Location = new System.Drawing.Point(6, 45);
+            this.BtnRun.Name = "BtnRun";
+            this.BtnRun.Size = new System.Drawing.Size(114, 23);
+            this.BtnRun.TabIndex = 1;
+            this.BtnRun.Text = "Run";
+            this.BtnRun.UseVisualStyleBackColor = true;
+            this.BtnRun.Click += new System.EventHandler(this.BtnRun_Click);
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(6, 19);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(383, 20);
+            this.txtKey.TabIndex = 0;
+            this.txtKey.Text = "token";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 105);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "PrintBot control";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BtnRun;
+        private System.Windows.Forms.TextBox txtKey;
+    }
+}
+
---
+++ revcom_bot/Form1.cs
@@ -0,0 +1,162 @@
+﻿using RawPrint;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrinterBot
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bw;
+
+        private static Random random = new Random();
+        private string printText;
+        private string printer = "\\\\pi\\HP_P1102_Office";
+
+        static IEnumerable<string> Compact(string str, int chunkSize) {
+            return Enumerable.Range(0, str.Length / chunkSize)
+                .Select(i => str.Substring(i * chunkSize, (i * chunkSize + chunkSize <= str.Length) ? chunkSize : str.Length - i * chunkSize));
+        }
+
+        public static string RandomString(int length) {
+            const string chars = "abcdefghijklmnopqrstuvwxyz";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        
+
+        public Form1()
+        {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+
+            //
+            // TODO: Add constructor code after the InitializeComponent() call.
+            //
+
+            this.bw = new BackgroundWorker();
+            this.bw.DoWork += bw_DoWork;
+        }
+
+        async void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var worker = sender as BackgroundWorker;
+            var key = e.Argument as String; // получаем ключ из аргументов
+            try
+            {
+                var Bot = new Telegram.Bot.TelegramBotClient(key); // инициализируем API
+                await Bot.SetWebhookAsync(""); // Обязательно! убираем старую привязку к вебхуку для бота
+
+                Bot.OnUpdate += async (object su, Telegram.Bot.Args.UpdateEventArgs evu) =>
+                {
+                    if (evu.Update.CallbackQuery != null || evu.Update.InlineQuery != null) return; // в этом блоке нам келлбэки и инлайны не нужны
+                    var update = evu.Update;
+                    var message = update.Message;
+                    if (message == null) return;
+                    if (message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+                    {
+                        if (message.Text == "/start") {
+                            string hi = "Hello!\nI'm PrintBot!\nType or forward any text or PDF document and I'll print it on " + printer + "."; 
+                            await Bot.SendTextMessageAsync(message.Chat.Id, hi, replyToMessageId: message.MessageId);
+                        }
+                        else {
+                            await Bot.SendTextMessageAsync(message.Chat.Id, "Printing", replyToMessageId: message.MessageId);
+                            printText = message.Text;
+                            //Print("HP_P1102_Office on pi", "dfd");
+                            Print(printer);
+                            await Bot.SendTextMessageAsync(message.Chat.Id, "Printed", replyToMessageId: message.MessageId);
+                        }
+                    } else if (message.Type == Telegram.Bot.Types.Enums.MessageType.Document) {
+                        if (message.Document.FileName.Contains("pdf")) {
+                            await Bot.SendTextMessageAsync(message.Chat.Id, "Downloading", replyToMessageId: message.MessageId);
+                            var outp = "";
+                            DownloadFile(message.Document.FileId, Environment.GetEnvironmentVariable("appdata"), Bot, key, out outp);
+                            await Bot.SendTextMessageAsync(message.Chat.Id, "Printing", replyToMessageId: message.MessageId);
+                            // Create an instance of the Printer
+                            IPrinter iprinter = new Printer();
+                            // Print the file
+                            iprinter.PrintRawFile(printer, outp, "PrintBot job");
+                            await Bot.SendTextMessageAsync(message.Chat.Id, "Printed", replyToMessageId: message.MessageId);
+                        } else {
+                            await Bot.SendTextMessageAsync(message.Chat.Id, "That... is... not... a PDF.", replyToMessageId: message.MessageId);
+                        }
+                    }
+                };
+
+                // запускаем прием обновлений
+                Bot.StartReceiving();
+            }
+            catch (Telegram.Bot.Exceptions.ApiRequestException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void BtnRun_Click(object sender, EventArgs e)
+        {
+            var text = @txtKey.Text; // получаем содержимое текстового поля txtKey в переменную text
+            if (text != "" && this.bw.IsBusy != true)
+            {
+                this.bw.RunWorkerAsync(text); // передаем эту переменную в виде аргумента методу bw_DoWork
+                BtnRun.Text = "Porg is running...";
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Print(string PrinterName) {
+            PrintDocument doc = new PrintDocument();
+            doc.PrinterSettings.PrinterName = PrinterName;
+            doc.PrintPage += new PrintPageEventHandler(PrintHandler);
+            doc.Print();
+        }
+
+        private void PrintHandler(object sender, PrintPageEventArgs ppeArgs) {
+            Font FontNormal = new Font("Verdana", 12);
+            Graphics g = ppeArgs.Graphics;
+            string outp = "";
+            g.DrawString(printText, FontNormal, Brushes.Black, 0, 20, new StringFormat());
+        }
+
+        private static void DownloadFile(string fileId, string path, Telegram.Bot.TelegramBotClient bot, string key, out string destination) {
+            var test = bot.GetFileAsync(fileId);
+            var download_url = @"https://api.telegram.org/file/bot" + key + "//" + test.Result.FilePath;
+            var localPath = path + "\\tgprint\\" + test.Result.FilePath;
+
+            if (!System.IO.Directory.Exists(localPath + "TMP"))
+                System.IO.Directory.CreateDirectory(localPath + "TMP");
+
+            using (StreamWriter file =
+            new StreamWriter(localPath)) {
+                file.WriteLine("");
+            }
+            using (WebClient client = new WebClient()) {
+                client.DownloadFile(new Uri(download_url), localPath);
+            }
+            destination = localPath;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+    }
+
+}
