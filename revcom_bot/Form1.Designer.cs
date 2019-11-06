121352b43bc0114f43534f9bb94984564b65320c
Narek Torosyan <narek.email@gmail.com>
2019-11-06 16:45:46 +0400

    Update the form's look

--- revcom_bot/Form1.Designer.cs
+++ revcom_bot/Form1.Designer.cs
@@ -28,32 +28,25 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.BtnRun = new System.Windows.Forms.Button();
             this.txtKey = new System.Windows.Forms.TextBox();
+            this.BtnRun = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.testpage = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.BtnRun);
             this.groupBox1.Controls.Add(this.txtKey);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(395, 77);
+            this.groupBox1.Size = new System.Drawing.Size(395, 52);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Token";
+            this.groupBox1.Text = "Telegram bot token";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
-            // 
-            // BtnRun
-            // 
-            this.BtnRun.Location = new System.Drawing.Point(6, 45);
-            this.BtnRun.Name = "BtnRun";
-            this.BtnRun.Size = new System.Drawing.Size(114, 23);
-            this.BtnRun.TabIndex = 1;
-            this.BtnRun.Text = "Run";
-            this.BtnRun.UseVisualStyleBackColor = true;
-            this.BtnRun.Click += new System.EventHandler(this.BtnRun_Click);
             // 
             // txtKey
             // 
@@ -61,19 +54,61 @@
             this.txtKey.Name = "txtKey";
             this.txtKey.Size = new System.Drawing.Size(383, 20);
             this.txtKey.TabIndex = 0;
-            this.txtKey.Text = "token";
+            this.txtKey.Text = "Insert token from @BotFather";
+            // 
+            // BtnRun
+            // 
+            this.BtnRun.Location = new System.Drawing.Point(18, 128);
+            this.BtnRun.Name = "BtnRun";
+            this.BtnRun.Size = new System.Drawing.Size(185, 23);
+            this.BtnRun.TabIndex = 1;
+            this.BtnRun.Text = "Run";
+            this.BtnRun.UseVisualStyleBackColor = true;
+            this.BtnRun.Click += new System.EventHandler(this.BtnRun_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 70);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(395, 52);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Printer name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(383, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Full name, if on network include the path.";
+            // 
+            // testpage
+            // 
+            this.testpage.Location = new System.Drawing.Point(209, 128);
+            this.testpage.Name = "testpage";
+            this.testpage.Size = new System.Drawing.Size(192, 23);
+            this.testpage.TabIndex = 2;
+            this.testpage.Text = "Print test page";
+            this.testpage.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(419, 105);
+            this.ClientSize = new System.Drawing.Size(419, 165);
+            this.Controls.Add(this.testpage);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.BtnRun);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "PrintBot control";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -83,6 +118,9 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button BtnRun;
         private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button testpage;
     }
 }
 
